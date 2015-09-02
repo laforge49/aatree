6ec0249394e01bd33ad442ba2cb9ae6a1027ad3b
Bill La Forge <laforge49@gmail.com>
2015-09-02 15:28:38 -0400

    assoc, seq

--- src/aatree/AAMap.clj
+++ src/aatree/AAMap.clj
@@ -2,7 +2,7 @@
   (:gen-class
     :main false
     :extends clojure.lang.APersistentMap
-    :implements [clojure.lang.IObj]
+    :implements [clojure.lang.IObj clojure.lang.Reversible clojure.lang.Sorted]
     :constructors {[]
                    []
                    [java.util.Comparator]
@@ -12,10 +12,10 @@
                    [clojure.lang.IPersistentMap java.lang.Object aatree.nodes.IMapNode]
                    []}
     :init init
-    :state state
-    )
-    (:require [aatree.nodes :refer :all])
-    (:import (aatree AAMap)))
+    :state state)
+  (:require [aatree.nodes :refer :all])
+  (:import (aatree AAMap)
+           (clojure.lang MapEntry)))
 
 
 (deftype map-state [node meta])
@@ -46,3 +46,26 @@
        (.getValue e))))
   ([this key]
    (-valAt this key nil)))
+
+(defn -assoc [this key val]
+  (let [n0 (.-node (.-state this))
+        n1 (.insert n0 (new MapEntry key val))]
+    (if (identical? n0 n1)
+      this
+      (new AAMap (.-meta (.-state this)) nil n1))))
+
+(defn -assocEx [this key val]
+  (let [n0 (.-node (.-state this))]
+    (if (-containsKey this key)
+      this
+      (new AAMap (.-meta (.-state this)) nil (.insert n0 (new MapEntry key val))))))
+
+(defn -without [this key]
+  (let [n0 (.-node (.-state this))
+        n1 (.delete n0 key)]
+    (if (identical? n0 n1)
+      this
+      (new AAMap (.-meta (.-state this)) nil n1))))
+
+(defn -seq [this]
+  (new-map-entry-seq (.-node (.-state this))))
