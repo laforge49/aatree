7b7d99928b37fdcbcccad64054e819600398b594
Bill La Forge <laforge49@gmail.com>
2015-09-07 20:16:00 -0400

    map-state is record

--- src/aatree/AAMap.clj
+++ src/aatree/AAMap.clj
@@ -23,7 +23,7 @@
            (clojure.lang MapEntry)))
 
 
-(deftype map-state [node meta])
+(defrecord map-state [node meta])
 
 (defn -init
   ([]
@@ -35,11 +35,11 @@
   ([meta _ node]
    [[] (->map-state node meta)]))
 
-(defn -meta [this] (.-meta (.-state this)))
+(defn -meta [this] (:meta (.-state this)))
 
-(defn -withMeta [this meta] (new AAMap meta nil (.-node (.-state this))))
+(defn -withMeta [this meta] (new AAMap meta nil (:node (.-state this))))
 
-(defn -entryAt [this key] (get-t2 (.-node (.-state this)) key))
+(defn -entryAt [this key] (get-t2 (:node (.-state this)) key))
 
 (defn -containsKey [this key] (boolean (-entryAt this key)))
 
@@ -53,65 +53,65 @@
    (-valAt this key nil)))
 
 (defn -assoc [this key val]
-  (let [n0 (.-node (.-state this))
+  (let [n0 (:node (.-state this))
         n1 (insert n0 (new MapEntry key val))]
     (if (identical? n0 n1)
       this
-      (new AAMap (.-meta (.-state this)) nil n1))))
+      (new AAMap (:meta (.-state this)) nil n1))))
 
 (defn -assocEx [this key val]
-  (let [n0 (.-node (.-state this))]
+  (let [n0 (:node (.-state this))]
     (if (-containsKey this key)
       this
-      (new AAMap (.-meta (.-state this)) nil (insert n0 (new MapEntry key val))))))
+      (new AAMap (:meta (.-state this)) nil (insert n0 (new MapEntry key val))))))
 
 (defn -without [this key]
-  (let [n0 (.-node (.-state this))
+  (let [n0 (:node (.-state this))
         n1 (del n0 key)]
     (if (identical? n0 n1)
       this
-      (new AAMap (.-meta (.-state this)) nil n1))))
+      (new AAMap (:meta (.-state this)) nil n1))))
 
 (defn -rseq [this]
-  (new-counted-reverse-seq (.-node (.-state this))))
+  (new-counted-reverse-seq (:node (.-state this))))
 
 (defn -seq
   ([this]
-   (new-counted-seq (.-node (.-state this))))
+   (new-counted-seq (:node (.-state this))))
   ([this ascending]
    (if ascending
      (-seq this)
      (-rseq this))))
 
 (defn -keyIterator [this]
-  (new-map-key-seq (.-node (.-state this))))
+  (new-map-key-seq (:node (.-state this))))
 
 (defn -valIterator [this]
-  (new-map-value-seq (.-node (.-state this))))
+  (new-map-value-seq (:node (.-state this))))
 
 (defn -seqFrom [this key ascending]
   (if ascending
-    (new-map-entry-seq (.-node (.-state this)) key)
-    (new-map-entry-reverse-seq (.-node (.-state this)) key)))
+    (new-map-entry-seq (:node (.-state this)) key)
+    (new-map-entry-reverse-seq (:node (.-state this)) key)))
 
 (defn -empty [this]
-  (new AAMap (.-meta (.-state this)) nil (empty-node (.-node (.-state this)))))
+  (new AAMap (:meta (.-state this)) nil (empty-node (:node (.-state this)))))
 
 (defn -count [this]
-  (:cnt (.-node (.-state this))))
+  (:cnt (:node (.-state this))))
 
 (defn -comparator [this]
-  (:comparator (.-node (.-state this))))
+  (:comparator (:node (.-state this))))
 
 (defn -entryKey [this entry]
   (.getKey entry))
 
 (defn -iterator [this]
-  (new-counted-iterator (.-node (.-state this))))
+  (new-counted-iterator (:node (.-state this))))
 
 (defn -nth
   ([this i]
-   (nth-t2 (.-node (.-state this)) i))
+   (nth-t2 (:node (.-state this)) i))
   ([this i notFound]
    (if (and (>= i 0) (< i (.count this)))
      (.nth this i)
