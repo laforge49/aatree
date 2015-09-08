cba32bf8d60173f13518d19da1a82ee0e33f5e6c
Bill La Forge <laforge49@gmail.com>
2015-09-08 14:14:23 -0400

    cleanup

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :aot [aatree.CountedSequence aatree.nodes aatree.map-nodes aatree.vector-nodes aatree.AAMap]
+  :warn-on-reflection true
   :profiles {:dev {:dependencies [[collection-check "0.1.6"]]}})
--- src/aatree/CountedSequence.clj
+++ src/aatree/CountedSequence.clj
@@ -10,42 +10,46 @@
     :init init
     :state state
     :methods [^:static [create [java.util.Iterator clojure.lang.IFn] Object]])
-  )
+  (:import (java.util Iterator)
+           (clojure.lang Counted)
+           (aatree CountedSequence)))
 
-(defn -create [iter styp]
+(defn -create [^Iterator iter styp]
   (if (.hasNext iter)
     (new aatree.CountedSequence iter styp)
     nil))
 
-(deftype seq-state [iter styp val rst])
+(defrecord seq-state [^Iterator iter styp val rst])
+
+(defn iter ^Iterator [seq-state] (:iter seq-state))
 
 (defn -init
-  ([iter styp]
+  ([^Iterator iter styp]
    (let [s (->seq-state iter styp (atom nil) (atom nil))]
-     (reset! (.-val s) s)
-     (reset! (.-rst s) s)
+     (reset! (:val s) s)
+     (reset! (:rst s) s)
      [[] s]))
   ([meta t s]
    [[meta] s])
   )
 
-(defn -withMeta [this meta] (new aatree.CountedSequence meta nil (.-state this)))
+(defn -withMeta [^CountedSequence this meta] (new aatree.CountedSequence meta nil (.-state this)))
 
-(defn -first [this]
+(defn -first [^CountedSequence this]
   (let [s (.-state this)
-        v (.-val s)]
+        v (:val s)]
     (if (= s @v)
-      (swap! v #(if (= s %) (.next (.-iter s)))))
-    (apply (.-styp (.state this)) [@(.-val s)])))
+      (swap! v #(if (= s %) (.next (iter s)))))
+    (apply (:styp s) [@(:val s)])))
 
-(defn -next [this]
+(defn -next [^CountedSequence this]
   (let [s (.-state this)
-        r (.-rst s)]
+        r (:rst s)]
     (when (= s @r)
       (-first this)
-      (swap! r #(if (= s %) (-create (.-iter s) (.-styp (.-state this))))))
-    @(.-rst s)))
+      (swap! r #(if (= s %) (-create (:iter s) (:styp s)))))
+    @(:rst s)))
 
-(defn -count [this]
-  (let [iter (.iter (.-state this))]
+(defn -count [^CountedSequence this]
+  (let [^Counted iter (:iter (.-state this))]
       (.count iter)))
