01a4915cf640e50ea9c633b79ddcfe43c4cb893a
Max Countryman <maxc@me.com>
2015-09-12 10:58:38 -0700

    provide cljfmt as a leiningen plugin

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :aot [aatree.CountedSequence aatree.nodes aatree.AAMap aatree.AAVector]
+  :plugins  [[lein-cljfmt "0.3.0"]]
   :profiles {:dev {:dependencies [[collection-check "0.1.6"]]}})
