2d94094d49f24788237c9e46eef7c9e9b6ee47b7
Bill La Forge <laforge49@gmail.com>
2015-09-28 19:12:50 -0400

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aatree "0.2.0"
+(defproject aatree "0.3.0"
   :description "A Clojure library for AA Trees"
   :url "https://github.com/laforge49/aatree"
   :license {:name "Eclipse Public License"
