12e19584c8cd288b425a911476e41c668890ba2a
Bill La Forge <laforge49@gmail.com>
2015-10-05 16:01:01 -0400

    lazy vector examples

---
+++ test/aatree/lazy_vector_examples.clj
@@ -0,0 +1,17 @@
+(ns aatree.lazy-vector-examples
+  (:require [aatree.core :refer :all])
+  (:import (java.nio ByteBuffer)))
+
+(println (lazy-byte-length emptyLazyAAVector))
+
+(def lv1 (conj emptyLazyAAVector 1 2 3)); -> 1
+(println lv1); -> [1 2 3]
+
+(def lv1-len (lazy-byte-length lv1))
+(println lv1-len); -> 61
+
+(def bb (ByteBuffer/allocate lv1-len))
+(lazy-write lv1 bb)
+(.flip bb)
+(def lv2 (load-aavector bb))
+(println lv2); -> 61
