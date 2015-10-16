57a14a5c36251907817da8c2ef64800b12292986
Bill La Forge <laforge49@gmail.com>
2015-10-16 18:08:54 -0400

    rename benchmarks

--- test/aatree/lazy_map_benchmark.clj
+++ test/aatree/lazy_map_benchmark.clj
@@ -1,30 +1,33 @@
-(ns aatree.vector-updates
+(ns aatree.lazy-map-benchmark
   (:require [aatree.core :refer :all])
   (:import (java.nio ByteBuffer)))
 
 (set! *warn-on-reflection* true)
 
-(def vector-size 1000000)
+(def map-size 1000000)
 (def updates 1000)
 
+(defn bld [m i]
+  (conj m [i i]))
+
 (println)
 (def t0 (System/currentTimeMillis))
-(def lazy-vector (reduce conj (new-vector (lazy-opts)) (range vector-size)))
+(def lazy-map (reduce bld (new-sorted-map (lazy-opts)) (range map-size)))
 (def t1 (System/currentTimeMillis))
 (def micr-0 (* 1000. (- t1 t0)))
-(println "Time to build a lazy vector of size" vector-size "=" micr-0 "microseconds")
-(println "Time per entry:" (/ micr-0 vector-size) "microseconds")
+(println "Time to build a lazy sorted map of size" map-size "=" micr-0 "microseconds")
+(println "Time per entry:" (/ micr-0 map-size) "microseconds")
 
-(defn upd [v i]
-  (let [v1 (assoc v i (- i))
-        bb (ByteBuffer/allocate (byte-length v1))]
-    (put-bytebuffer v1 bb)
+(defn upd [m i]
+  (let [m1 (assoc m i (- i))
+        bb (ByteBuffer/allocate (byte-length m1))]
+    (put-bytebuffer m1 bb)
     (.flip bb)
-    (load-lazy-vector bb)))
+    (load-lazy-map bb)))
 
 (println)
 (def t0 (System/currentTimeMillis))
-(def lazy-vector (reduce upd lazy-vector (range updates)))
+(def lazy-m (reduce upd lazy-map (range updates)))
 (def t1 (System/currentTimeMillis))
 (def micr-0 (* 1000. (- t1 t0)))
 (println "Time to deserialize/update/reserialize " updates "times =" micr-0 "microseconds")
