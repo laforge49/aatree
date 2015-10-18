738641c4f111c8858b8aaa9305a3d46e0fb70ee6
Bill La Forge <laforge49@gmail.com>
2015-10-18 12:39:55 -0400

    more cleanup

--- test/aatree/file_example.clj
+++ test/aatree/file_example.clj
@@ -6,13 +6,14 @@
 (set! *warn-on-reflection* true)
 
 (def opts (lazy-opts))
+(def empty-map (new-sorted-map opts))
 
-(def bm1 (conj (new-sorted-map opts) {:dog "Jack" :cat "Sammy" :rabbit "Henry"}))
-(def bm1-len (byte-length bm1))
-(def ^ByteBuffer bb (ByteBuffer/allocate bm1-len))
-(put-bytebuffer bm1 bb)
-(.flip bb)
-(file-save bb (File. "file-example.lazy"))
+(let [bm1 (conj empty-map {:dog "Jack" :cat "Sammy" :rabbit "Henry"})
+      bm1-len (byte-length bm1)
+      ^ByteBuffer bb (ByteBuffer/allocate bm1-len)]
+  (put-bytebuffer bm1 bb)
+  (.flip bb)
+  (file-save bb (File. "file-example.lazy")))
 
 (let [^ByteBuffer bb (file-load (File. "file-example.lazy"))
       bm2 (load-sorted-map bb opts)]
