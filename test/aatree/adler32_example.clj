a7a952d09bbb38d487a5d3491eaf44e1c172d1e9
Bill La Forge <laforge49@gmail.com>
2015-10-18 12:35:35 -0400

    cleanup

--- test/aatree/adler32_example.clj
+++ test/aatree/adler32_example.clj
@@ -8,17 +8,17 @@
 
 (def opts (lazy-opts))
 (def empty-vec (new-vector opts))
-(def lv1 (conj empty-vec 1 2 3))
 
-(def bb-len (+ (byte-length lv1) 8))
-(def ^ByteBuffer bb (ByteBuffer/allocate bb-len))
-(put-bytebuffer lv1 bb)
-(def ^Adler32 adler32 (Adler32.))
-(def ^ByteBuffer abb (.flip (.duplicate bb)))
-(.update adler32 abb)
-(.putLong bb (.getValue adler32))
-(.flip bb)
-(file-save bb (File. "adler32-example.lazy"))
+(let [lv1 (conj empty-vec 1 2 3)
+      bb-len (+ (byte-length lv1) 8)
+      ^ByteBuffer bb (ByteBuffer/allocate bb-len)
+      _ (put-bytebuffer lv1 bb)
+      ^Adler32 adler32 (Adler32.)
+      ^ByteBuffer abb (.flip (.duplicate bb))]
+  (.update adler32 abb)
+  (.putLong bb (.getValue adler32))
+  (.flip bb)
+  (file-save bb (File. "adler32-example.lazy")))
 
 (let [^ByteBuffer bb (file-load (File. "adler32-example.lazy"))
       csp (- (.limit bb) 8)
