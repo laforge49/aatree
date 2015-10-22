a975f56ce9df9b8e64026ddf68450448b4deb081
Bill La Forge <laforge49@gmail.com>
2015-10-22 11:10:43 -0400

    data -> aamap

--- src/aatree/calf.clj
+++ src/aatree/calf.clj
@@ -10,20 +10,20 @@
 
 (defn- calf-updater [db-state app-updater opts]
   (try
-    (let [aamap (app-updater (:data db-state) opts)
+    (let [aamap (app-updater (:aamap db-state) opts)
           transaction-count (:transaction-count db-state)
           block-size (:block-size opts)
           position (* block-size (mod transaction-count 2))
           transaction-count (+ transaction-count 1)
           db-state (assoc db-state :transaction-count transaction-count)
-          db-state (assoc db-state :data aamap)
+          db-state (assoc db-state :aamap aamap)
           ^ByteBuffer bb (ByteBuffer/allocate block-size)
           ^FileChannel file-channel (:file-channel opts)
-          data-size (byte-length aamap)]
-      (if (< block-size (+ 4 4 8 data-size 32))
+          map-size (byte-length aamap)]
+      (if (< block-size (+ 4 4 8 map-size 32))
         (throw (Exception. "block-size exceeded on write")))
       (.putInt bb block-size)
-      (.putInt bb data-size)
+      (.putInt bb map-size)
       (.putLong bb transaction-count)
       (put-aa bb aamap)
       (put-cs256 bb (compute-cs256 (.flip (.duplicate bb))))
@@ -54,7 +54,7 @@
   aamap)
 
 (defn- calf-new [opts]
-  (let [db-state {:transaction-count 0 :data (new-sorted-map opts)}
+  (let [db-state {:transaction-count 0 :aamap (new-sorted-map opts)}
         opts (create-db-agent db-state opts)]
     (calf-update calf-null-updater opts)
     (calf-update calf-null-updater opts)
@@ -70,11 +70,11 @@
         _ (.flip bb)]
     (if (not= block-size (.getInt bb))
       nil
-      (let [data-size (.getInt bb)
-            _ (if (< block-size (+ 4 4 8 data-size 32))
+      (let [map-size (.getInt bb)
+            _ (if (< block-size (+ 4 4 8 map-size 32))
                 (throw (Exception. "block-size exceeded on read")))
             transaction-count (.getLong bb)
-            input-size (+ (.limit bb) data-size 32)
+            input-size (+ (.limit bb) map-size 32)
             _ (.limit bb input-size)
             _ (.read file-channel bb)
             _ (.flip bb)
@@ -85,10 +85,10 @@
             ocs (get-cs256 bb)
             _ (.position bb (+ 4 4 8))
             _ (.limit bb csp)
-            data (load-sorted-map bb opts)]
+            aamap (load-sorted-map bb opts)]
         (if (not= cs ocs)
           nil
-          {:transaction-count transaction-count :data data})))))
+          {:transaction-count transaction-count :aamap aamap})))))
 
 (defn- choose [state0 state1]
   (if state0
@@ -134,8 +134,8 @@
 (defn calf-transaction-count [opts]
   (:transaction-count @(:db-agent opts)))
 
-(defn calf-get [opts]
-  (:data @(:db-agent opts)))
+(defn calf-get-map [opts]
+  (:aamap @(:db-agent opts)))
 
 (defn calf-close [opts]
   (let [^FileChannel fc (:file-channel opts)]
