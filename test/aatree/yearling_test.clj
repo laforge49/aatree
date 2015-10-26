6e5ae8f3bae7a354233c39f5f0600bf61c129fe2
Bill La Forge <laforge49@gmail.com>
2015-10-26 09:51:23 -0400

    yearling test works

---
+++ test/aatree/yearling_test.clj
@@ -0,0 +1,28 @@
+(ns aatree.yearling-test
+  (:require [clojure.test :refer :all]
+            [aatree.core :refer :all]
+            [aatree.yearling :refer :all])
+  (:import (java.io File)))
+
+(set! *warn-on-reflection* true)
+
+(deftest yearling
+  (.delete (File. "yearling-test.yearling"))
+
+  (let [opts (yearling-open (File. "yearling-test.yearling") 10000 100000)
+        _ (is (= (db-transaction-count opts) 2))
+        aamap (db-get-sorted-map opts)
+        _ (is (= aamap {}))
+        _ (db-update (fn [aamap opts]
+                       (assoc aamap :fun "Clojure"))
+                     opts)
+        aamap (db-get-sorted-map opts)
+        _ (is (= aamap {:fun "Clojure"}))
+        _ (is (= (db-transaction-count opts) 3))
+        _ (db-close opts)])
+
+  (let [opts (yearling-open (File. "yearling-test.yearling") 10000 100000)
+        _ (is (= (db-transaction-count opts) 3))
+        aamap (db-get-sorted-map opts)
+        _ (is (= aamap {:fun "Clojure"}))
+        _ (db-close opts)]))
