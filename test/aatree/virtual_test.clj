58cfd88e02a813eeb782c4e56237176c2698d110
Bill La Forge <laforge49@gmail.com>
2015-11-01 06:31:20 -0500

    a better test

--- test/aatree/virtual_test.clj
+++ test/aatree/virtual_test.clj
@@ -9,48 +9,19 @@
 (deftest virtual
   (.delete (File. "virtual-test.yearling"))
 
-  (let [opts (yearling-open (File. "virtual-test.yearling") 400 100000
+  (let [opts (yearling-open (File. "virtual-test.yearling") 10000 20000000
                             {:send-update-timeout 300})]
-    (db-update (fn [aamap opts]
-                 (assoc aamap :a 1))
-               opts)
-    (println 1 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :b 2))
-               opts)
-    (println 2 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :c 3))
-               opts)
-    (println 3 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :d 4))
-               opts)
-    (println 4 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :e 5))
-               opts)
-    (println 5 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :f 6))
-               opts)
-    (println 6 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :f 6))
-               opts)
-    (println 7 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :f 6))
-               opts)
-    (println 8 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :f 6))
-               opts)
-    (println 9 (db-allocated opts) (count (db-release-pending opts)))
-    (db-update (fn [aamap opts]
-                 (assoc aamap :f 6))
-               opts)
-    (println 10 (db-allocated opts) (count (db-release-pending opts)))
+    (reduce (fn [_ j]
+              (db-update (fn [aamap opts]
+                           (reduce (fn [m i]
+                                     (assoc m (+ i (* j 100000)) 1))
+                                   aamap
+                                   (range 1000)))
+                         opts)
+              )
+            0
+            (range 100))
+;    (println 1 (db-allocated opts) (count (db-release-pending opts)))
     (db-close opts))
 
   (Thread/sleep 200))
