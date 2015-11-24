d3778d09d0b35f97f5d518447edd1b78ac5be095
Bill La Forge <laforge49@gmail.com>
2015-11-24 12:47:57 -0500

    extending with protocols

---
+++ test/aatree/record_play.clj
@@ -0,0 +1,38 @@
+(ns aatree.record-play)
+
+(set! *warn-on-reflection* true)
+
+(defrecord base [])
+
+(defn new-base [opts]
+  (let [b (->base)
+        b (into b opts)
+        b (assoc b :blap (fn [this] 42))]
+    b))
+
+(defrecord wackel [])
+
+(defn new-wackel [opts]
+  (let [w (->wackel)
+        w (into w opts)
+        w (assoc w :blip (fn [this x y z] (+ x y z)))]
+    w))
+
+(defprotocol gran
+  (blip [this x y z])
+  (blap [this]))
+
+(def w (-> {} new-base new-wackel))
+
+(extend wackel
+  gran
+  {:blip (fn [this x y z]
+           ((:blip this) this x y z))
+   :blap (fn [this]
+           ((:blap this) this))})
+
+(def w (-> {} new-base new-wackel))
+
+(println (blip w 1 2 3)); -> 6
+
+(println (blap w)); -> 42
