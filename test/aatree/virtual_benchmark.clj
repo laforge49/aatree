cc41314ad8ec2c68f7dc666458226e78b65743ff
Bill La Forge <laforge49@gmail.com>
2015-11-27 19:02:59 -0500

    virtual benchmark works

--- test/aatree/virtual_benchmark.clj
+++ test/aatree/virtual_benchmark.clj
@@ -1,17 +1,16 @@
 (ns aatree.virtual-benchmark
   (:require [clojure.test :refer :all]
             [aatree.core :refer :all]
-            [aatree.yearling :refer :all])
+            [aatree.yearling :refer :all]
+            [aatree.closer-trait :refer :all])
   (:import (java.io File)))
 
 (set! *warn-on-reflection* true)
 
-(comment
-
   (deftest virtual
     (.delete (File. "virtual-benchmark.yearling"))
 
-    (let [opts (yearling-open (File. "virtual-benchmark.yearling"))
+    (let [yearling (yearling-open (File. "virtual-benchmark.yearling"))
           ;        mxi 100000
           mxi 3
           mxj 5
@@ -21,24 +20,23 @@
           (fn [_ k]
             (reduce
               (fn [_ j]
-                (db-update (fn [aamap opts]
-                             (let [bbmap (reduce (fn [m i]
-                                                   (assoc m (+ i (* mxi j) (* mxi mxj k)) 1))
-                                                 aamap
-                                                 (range mxi)
-                                                 )]
-                               bbmap))
-                           opts))
+                (db-update
+                  yearling
+                  (fn [yearling aamap]
+                    (let [bbmap (reduce (fn [m i]
+                                          (assoc m (+ i (* mxi j) (* mxi mxj k)) 1))
+                                        aamap
+                                        (range mxi))]
+                      bbmap))))
               nil
               (range mxj)))
           nil
-          (range mxk))) ; -> "Elapsed time: 39369.950654 msecs"
-      (println "count" (count (db-get-sorted-map opts)))
+          (range mxk)))                                     ; -> "Elapsed time: 39369.950654 msecs"
+      (println "count" (count (db-get-sorted-map yearling)))
       (time (reduce
-              (fn [_ i] (get (db-get-sorted-map opts) i))
+              (fn [_ i] (get (db-get-sorted-map yearling) i))
               nil
-              (range (count (db-get-sorted-map opts))))) ; -> "Elapsed time: 8404.581527 msecs"
-      (db-close opts))
+              (range (count (db-get-sorted-map yearling))))) ; -> "Elapsed time: 8404.581527 msecs"
+      (do-close yearling))
 
     (Thread/sleep 200))
-  )
