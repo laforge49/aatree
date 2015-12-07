083a0b1e712044d91445ac21b006109d91d3b56a
Bill La Forge <laforge49@gmail.com>
2015-12-07 18:43:35 -0500

    0.6.1-snapshot, with core.async dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject aatree "0.6.0"
+(defproject aatree "0.6.1-SNAPSHOT"
   :description "A Clojure library for AA Trees"
   :url "https://github.com/laforge49/aatree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [medley "0.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.apache.logging.log4j/log4j-core "2.4.1"]
