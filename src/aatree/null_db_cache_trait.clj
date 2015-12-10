1f71a45394f400b995cd219e402156334c3ab0f8
Bill La Forge <laforge49@gmail.com>
2015-12-10 06:58:32 -0500

    added check-size

--- src/aatree/null_db_cache_trait.clj
+++ src/aatree/null_db_cache_trait.clj
@@ -1,15 +1,24 @@
 (ns aatree.null-db-cache-trait
-  (:require [aatree.core :refer :all]))
+  (:require [aatree.core :refer :all])
+  (:import (java.nio ByteBuffer)))
+
+(defn- check-size [this ^ByteBuffer byte-buffer]
+  (let [db-block-size (db-block-size this)
+        limit (.limit byte-buffer)]
+    (if (> limit db-block-size)
+      (throw (Exception. (str "byte buffer is too big:" limit))))))
 
 (defn null-db-cache [this]
   (-> this
       (assoc
         :block-read
-        (fn [this block-nbr byte-buffer]
+        (fn [this block-nbr ^ByteBuffer byte-buffer]
+          (check-size this byte-buffer)
           (db-file-read this byte-buffer (* block-nbr (db-block-size this)))))
       (assoc
         :block-write
-        (fn [this block-nbr byte-buffer]
+        (fn [this block-nbr ^ByteBuffer byte-buffer]
+          (check-size this byte-buffer)
           (db-file-write this byte-buffer (* block-nbr (db-block-size this)))))
       (assoc
         :block-clear
